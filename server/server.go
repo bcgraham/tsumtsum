781beba0c45444b30f4fa0cb6372f4f16cd1027d
bcgraham <bcgraham+github@gmail.com>
2014-11-23 19:35:30 -0500

    minor fix

--- server/server.go
+++ server/server.go
@@ -94,6 +94,7 @@ func main() {
 	router.GET("/tsum/:user/reports", get(reports))
 	router.POST("/tsum/:user/reports/searches", post(searches))
 	router.POST("/tsum/:user/reports/invites", post(invites))
+	fmt.Println("Listening...")
 	err = http.ListenAndServe(*port, router)
 	if err != nil {
 		fmt.Println(err)
